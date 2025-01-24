126822240fc33535f8089a77b39a8c624261dd88
Jilllllll <Ji@lllllll.com>
2025-01-24 22:34:04 +0800

    knock back ready

--- Assets/Scripts/Battle/Projectile.cs
+++ Assets/Scripts/Battle/Projectile.cs
@@ -6,6 +6,10 @@ namespace AmpFC.Battle
 {
     public class Projectile : MonoBehaviour
     {
+        public delegate void OnProjectileHit(float damage, float knockback, Vector2 direction);
+        public static OnProjectileHit onPlayerHit;
+        public static OnProjectileHit onEnemyHit;
+
         public float speed;
         private Vector2 _direction;
         public float lifeTime = 10;
@@ -32,9 +36,16 @@ namespace AmpFC.Battle
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
+        
             if (collision.CompareTag("Player"))
             {
-                //[TODO]: Apply knockback and damage to player
+                var direction = collision.transform.position - transform.position;
+                onPlayerHit?.Invoke(damage, knockback, direction);
+                Destroy(gameObject);
+            } else if (collision.CompareTag("Enemy"))
+            {
+                var direction = collision.transform.position - transform.position;
+                onEnemyHit?.Invoke(damage, knockback, direction);
                 Destroy(gameObject);
             }
         }
